77f1038822a4566e363ed4d8fcc56224acbda055
Rahul311-R <rahul541r@gmail.com>
2025-08-04 11:23:39 +0530

    init commit

--- all_even.java
+++ all_even.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class all_even {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+        System.out.print("Enter the number:");
         int n = s.nextInt();
         for(int i = 0 ; i <= n;i++ ){
             if(i%2==0){
