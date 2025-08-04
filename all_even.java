b73c1c76d6892c49811f1e3675da5a5c38e979cc
Rahul311-R <rahul541r@gmail.com>
2025-08-04 15:59:00 +0530

    new commit

--- all_even.java
+++ all_even.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class all_even {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+//rahul da
 
         int n = s.nextInt();
         for(int i = 0 ; i <= n;i++ ){
