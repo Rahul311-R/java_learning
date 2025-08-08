ef079f4fecaf9b3f94614aa45aacc93b4f083daa
Rahul311-R <rahul541r@gmail.com>
2025-08-08 16:11:14 +0530

    08/08/2025

--- binary_search.java
+++ binary_search.java
@@ -1,40 +1,42 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
-public class BinarySearch {
+public class binary_search {
 
-    public static int binary(int[] arr, int tar) {
+    static int binary(int[] arr,int tar){
         int left = 0;
-        int right = arr.length - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (arr[mid] == tar)
+        int right = arr.length-1;
+        while(left<=right){
+            int mid = left + (right - left) /2;
+            if(arr[mid]==tar){
                 return mid;
-            else if (arr[mid] < tar)
-                left = mid + 1;
-            else
+            }else if(arr[mid]<tar){
+                left = mid+1;
+            }
+            else{
                 right = mid - 1;
+            }
         }
         return -1;
     }
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.print("Enter the size of the array: ");
+        System.out.print("enter the size of the array:");
         int n = s.nextInt();
+        System.out.print("Enter "+n+" element of the array:");
         int[] arr = new int[n];
-
-        System.out.println("Enter " + n + " elements of the array:");
-        for (int i = 0; i < n; i++) {
-            arr[i] = s.nextInt();
+        for(int i = 0;i<n;i++){
+            arr[i]=s.nextInt();
         }
-
-        System.out.print("Enter element to search: ");
+        System.out.print("enter target element:");
         int tar = s.nextInt();
-
-        int result = binary(arr, tar);
-        if (result != -1)
-            System.out.println("Element found at index: " + result);
-        else
-            System.out.println("Element not found");
+        Arrays.sort(arr);
+        int a =binary(arr,tar);//func calling
+        if(a==-1){
+            System.out.println("element not found");
+        }else{
+            System.out.println("element found ");
+        }
     }
 }
